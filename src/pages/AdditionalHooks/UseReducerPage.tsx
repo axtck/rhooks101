73ba6f76e1425428e97433a5ad1fd4ab60849426
axtck <alexandertack@hotmail.be>
2021-06-21 20:41:32 +0200

    Add useReducer example

--- src/pages/AdditionalHooks/UseReducerPage.tsx
+++ src/pages/AdditionalHooks/UseReducerPage.tsx
@@ -1,18 +1,87 @@
-import React, { FunctionComponent, MouseEvent } from "react";
+import React, { FunctionComponent, MouseEvent, useReducer } from "react";
 import { useHistory } from "react-router-dom";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import ButtonGroupStacked from "../../components/Buttons/ButtonGroupStacked";
 import { Constants } from "../../constants";
+
+enum ActionType {
+    Increase = "INCREASE",
+    Decrease = "DECREASE"
+}
+
+interface ICounterState {
+    value: number;
+}
+
+const initialCounterState: ICounterState = {
+    value: 0
+};
+
+interface ICounterAction {
+    type: ActionType,
+    payload: number;
+}
+
+const increaseAction: ICounterAction = {
+    type: ActionType.Increase,
+    payload: 1
+};
+
+const decreaseAction: ICounterAction = {
+    type: ActionType.Decrease,
+    payload: 1
+};
+
+const counterReducer = (state: ICounterState, action: ICounterAction): ICounterState => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case ActionType.Increase:
+            return {
+                ...state,
+                value: state.value + payload
+            };
+        case ActionType.Decrease:
+            return {
+                ...state,
+                value: state.value - payload
+            };
+        default:
+            return state;
+    }
+};
+
 
 interface UseReducerPageProps { };
 
 const UseReducerPage: FunctionComponent<UseReducerPageProps> = () => {
 
+    const [counter, dispatch] = useReducer(counterReducer, initialCounterState);
+
     const history = useHistory();
 
-    const handleRouteClick = (e: MouseEvent<HTMLSpanElement>, route: string) => {
+    const handleRouteClick = (route: string) => {
         history.push(`/hooks/${route}`);
     };
+
+    const handleCounterActionClick = (e: MouseEvent<HTMLButtonElement>, action: string) => {
+        e.preventDefault();
+        if (action !== "+" && action !== "-") return;
+        if (action === "+") dispatch(increaseAction);
+        if (action === "-") dispatch(decreaseAction);
+    };
+
+    const btnDefs: IActionBtnDef[] = [
+        {
+            label: "increase",
+            action: "+"
+        },
+        {
+            label: "decrease",
+            action: "-"
+        }
+    ];
 
     return (
         <React.Fragment>
@@ -40,6 +109,46 @@ const UseReducerPage: FunctionComponent<UseReducerPageProps> = () => {
                 customStyle={Constants.highlightStyles}>
                 {`const [state, dispatch] = useReducer(reducer, initialArg, init);`}
             </SyntaxHighlighter>
+
+            <h5 className="mt-3">Examples</h5>
+            <p>This is how you would handle a counter with a reducer.</p>
+            <SyntaxHighlighter
+                language="javascript"
+                style={dracula}
+                customStyle={Constants.highlightStyles}>
+                {`const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            throw new Error();
+    }
+};
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <>
+            Count: {state.count}
+            <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button>
+            <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+        </>
+    );
+};`}
+            </SyntaxHighlighter>
+            <div className="row">
+                <div className="col-4">
+                    <ButtonGroupStacked btnDefs={btnDefs} onBtnClick={handleCounterActionClick} />
+                </div>
+                <div className="col-8 text-center">
+                    <h5>{counter.value}</h5>
+                </div>
+            </div>
+
             <h4 className="mt-4">Extra</h4>
             <h5 className="mt-3">Lazy initialization</h5>
             <p>
@@ -50,7 +159,7 @@ const UseReducerPage: FunctionComponent<UseReducerPageProps> = () => {
             <p>
                 If you return the same value from a Reducer Hook as the current state, React will bail out without rendering the children or firing effects. (React uses the Object.is comparison algorithm.)
                 Note that React may still need to render that specific component again before bailing out. That shouldn’t be a concern because React won’t unnecessarily go “deeper” into the tree.
-                If you’re doing expensive calculations while rendering, you can optimize them with <span className="link-success" role="button" onClick={(e) => handleRouteClick(e, "useMemo")}>useMemo</span>.
+                If you’re doing expensive calculations while rendering, you can optimize them with <span className="link-success" role="button" onClick={() => handleRouteClick("useMemo")}>useMemo</span>.
             </p>
         </React.Fragment>
     );
